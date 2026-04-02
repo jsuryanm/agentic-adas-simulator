67e1f621cc5d12fd9a170d9acad2f1e124b944df
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-04-03 01:34:05 +0800

    building scene_tool

---
+++ src/tools/scene_tool.py
@@ -0,0 +1,15 @@
+from typing import List,Tuple 
+from loguru import logger
+from src.exceptions.custom_exceptions import SceneToolException
+from src.models.schemas import SceneSummary
+
+
+class SceneTool:
+    
+    VEHICLE_LABELS = {"car",
+                      "truck",
+                      "bus",
+                      "rider",
+                      "cyclist"}
+    
+    PEDESTRIAN_LABELS = {"pedestrian"}
